d10db6d732ef6098a7a2e6772973a3fcce4c0939
dandidcee <dandi12cahyaman12@gmail.com>
2026-08-07 10:33:32 +0700

    fix: add cwd to backend pm2 config so it finds .env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "skyflow-backend",
-      script: "./backend/dist/index.js",
+      script: "./dist/index.js",
+      cwd: "./backend",
       instances: 1,
       autorestart: true,
       watch: false,
